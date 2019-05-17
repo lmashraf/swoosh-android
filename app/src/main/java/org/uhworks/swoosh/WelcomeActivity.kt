7d4e564b87d800c31ee8b43f88a9518f6764d18a
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2019-05-17 02:08:36 +0200

    - Added Intent to transition from MainActivity to LeagueActivity - Fixed hardcoded values in activities

--- app/src/main/java/org/uhworks/swoosh/WelcomeActivity.kt
+++ app/src/main/java/org/uhworks/swoosh/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package org.uhworks.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        startBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+
+            startActivity(leagueIntent)
+        }
     }
 }
