84a86ad3a2830b52ec0457469d57a4e9dae52b41
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2019-05-17 16:13:27 +0200

    Created sKill Activity transition. Fixed hardcoded values and warnings.

--- app/src/main/java/org/uhworks/swoosh/LeagueActivity.kt
+++ app/src/main/java/org/uhworks/swoosh/LeagueActivity.kt
@@ -1,12 +1,23 @@
 package org.uhworks.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View) {
+
+        // Create Intent
+        val skillActivity = Intent(this, SkillActivity::class.java)
+
+        // Start Activity
+        startActivity(skillActivity)
     }
 }
